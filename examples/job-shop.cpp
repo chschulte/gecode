fb56a0e66b4827c5254e2debc39ec2e45f7a0487
Christian Schulte <cschulte@kth.se>
2019-04-04 15:20:05 +0200

    Layout

--- examples/job-shop.cpp
+++ examples/job-shop.cpp
@@ -490,7 +490,7 @@ public:
       for (int i1=0; i1<n; i1++)
         for (int i2=i1+1; i2<n; i2++) {
           if (dur[j*n+i1] > dur[j*n+i2]) {
-                       order(*this,
+            order(*this,
                   start[jobs[j*n+i1]], dur[j*n+i1],
                   start[jobs[j*n+i2]], dur[j*n+i2],
                   sorder[l]);
